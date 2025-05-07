8c88aad70f7d565c722a30862f4a72c101b298bd
채정욱 <jeonguk95@gmail.com>
2025-05-07 23:33:35 +0900

    Vnkewsr

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { HashRouter } from "react-router-dom";  // ✅ 추가
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  
-    <App />
+  <React.StrictMode>
+    <HashRouter>  {/* ✅ 여기로 감싸줘야 Hash URL 처리됨 */}
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
