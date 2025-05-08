6ccd03ff9004028631dd3fec0ad6b337df2818e6
채정욱 <jeonguk95@gmail.com>
2025-05-08 17:56:51 +0900

    cnjwer

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: "/", // ✅ 배포 시 경로를 절대 루트로 고정
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // @/pages/Quiz 등
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
