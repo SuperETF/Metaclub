81fef1c664200e582df55267dda3052fa46504a5
채정욱 <jeonguk95@gmail.com>
2025-05-08 12:43:10 +0900

    vnkwer

--- src/layouts/ResponsiveContainer.tsx
+++ src/layouts/ResponsiveContainer.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const ResponsiveContainer = ({ children }) => {
   return (
-    <div className="w-full px-4 md:px-10 max-w-[100%] md:max-w-screen-lg mx-auto">
+    <div className="w-full px-4 sm:px-6 md:px-0 md:max-w-screen-md mx-auto">
       {children}
     </div>
   );
 };
+
 
 export default ResponsiveContainer;
